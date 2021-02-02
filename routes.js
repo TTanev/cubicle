beb5b24a39b64c516c69dc1494b4be1b0fc50b40
Toma <tomatanev@gmail.com>
2021-02-02 12:38:22 +0200

    routing

---
+++ routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express")
+const productController = require("./controllers/productController")
+const aboutController = require("./controllers/aboutController")
+
+const router = Router()
+
+router.use("/products", productController)
+router.use("/about", aboutController)
+
+module.exports = router
