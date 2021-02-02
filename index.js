a4d8210c44b9ba71a915c91c71aefaf7eb0f4056
Toma <tomatanev@gmail.com>
2021-02-02 11:25:32 +0200

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express")
-
+const handlebars = require("express-handlebars")
 const config = require("./config/config")
-
 const app = express()
+
+app.engine(
+  "hbs",
+  handlebars({
+    extname: "hbs",
+  })
+)
+
+app.use(express.static("public"))
+
+app.set("view engine", "hbs")
+
+app.get("/", (req, res) => {
+  res.render("home")
+})
 
 app.listen(config.PORT, () =>
   console.log(`Server is running on port ${config.PORT}...`)
